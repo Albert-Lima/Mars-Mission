7735f1d955b48720057a869f79d41e15589a6c98
Francisco Albert <albertsousalima@gmail.com>
2023-11-05 22:25:31 -0300

    Projeto Completo
    O site já está completo com JavaScript e CSS adicionados para uma experiência única e com boa funcionalidade no site.
    Agora resta apenas trabalhar a responsividade para dispositivos móves.

---
+++ arquivos/script.js
@@ -0,0 +1,34 @@
+var BttMostrarMais = document.querySelector('#bttTextMore')
+var TextoExploração = document.querySelector('#TextExploration')
+var BoxOfText = document.querySelector('#BoxTextExploration')
+
+function MostrarTexto(){
+    BttMostrarMais.innerText = 'mostrar menos'
+    TextoExploração.style.height = 'max-content'
+    BoxOfText.style.height = 'max-content'
+    BttMostrarMais.removeEventListener('click', MostrarTexto)
+    BttMostrarMais.addEventListener('click', MostrarMenos)
+    function MostrarMenos(){
+
+        BttMostrarMais.removeAttribute('style')
+        TextoExploração.removeAttribute('style')
+        BttMostrarMais.addEventListener('click', MostrarTexto)
+        BttMostrarMais.removeEventListener('click', MostrarMenos)
+    }
+}
+BttMostrarMais.addEventListener('click', MostrarTexto)
+
+
+
+var bttReturn = document.querySelector('#returnTop')
+var topo = document.querySelector('#topo')
+
+function ToTop(){
+    var topoRect = topo.getBoundingClientRect()
+    var centeryTop = topoRect.top + topoRect.height/2 - window.innerHeight/ 2
+    window.scrollTo({
+        top: centeryTop + window.scrollY,
+        behavior: 'smooth'
+    })
+}
+bttReturn.addEventListener('click', ToTop)
